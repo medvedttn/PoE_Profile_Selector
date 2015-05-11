22027a03501fadf323edec0d8e880c8b7b3e8f28
medvedttn <alien-tag@yandex.ru>
2015-05-11 20:41:32 +0300

    PoE Profile Selector v.1.0
    Initial release

---
+++ PoE Profile Selector/MainForm.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace PoE_Profile_Selector
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbProfiles = new System.Windows.Forms.ComboBox();
+            this.btnCreateProfile = new System.Windows.Forms.Button();
+            this.txtPoEpath = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbSelectFolder = new System.Windows.Forms.Button();
+            this.btnRunPoE = new System.Windows.Forms.Button();
+            this.btnDelProfile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select profile or create new:";
+            // 
+            // cmbProfiles
+            // 
+            this.cmbProfiles.FormattingEnabled = true;
+            this.cmbProfiles.Location = new System.Drawing.Point(158, 10);
+            this.cmbProfiles.MaxLength = 32;
+            this.cmbProfiles.Name = "cmbProfiles";
+            this.cmbProfiles.Size = new System.Drawing.Size(121, 21);
+            this.cmbProfiles.Sorted = true;
+            this.cmbProfiles.TabIndex = 1;
+            this.cmbProfiles.SelectedValueChanged += new System.EventHandler(this.cmbProfiles_SelectedValueChanged);
+            this.cmbProfiles.TextChanged += new System.EventHandler(this.cmbProfiles_TextChanged);
+            // 
+            // btnCreateProfile
+            // 
+            this.btnCreateProfile.Enabled = false;
+            this.btnCreateProfile.Location = new System.Drawing.Point(285, 2);
+            this.btnCreateProfile.Name = "btnCreateProfile";
+            this.btnCreateProfile.Size = new System.Drawing.Size(114, 34);
+            this.btnCreateProfile.TabIndex = 2;
+            this.btnCreateProfile.Text = "Create profile from current config";
+            this.btnCreateProfile.UseVisualStyleBackColor = true;
+            this.btnCreateProfile.Click += new System.EventHandler(this.btnCreateProfile_Click);
+            // 
+            // txtPoEpath
+            // 
+            this.txtPoEpath.Location = new System.Drawing.Point(109, 47);
+            this.txtPoEpath.Name = "txtPoEpath";
+            this.txtPoEpath.Size = new System.Drawing.Size(170, 20);
+            this.txtPoEpath.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Path to PoE exe:";
+            // 
+            // cmbSelectFolder
+            // 
+            this.cmbSelectFolder.Location = new System.Drawing.Point(285, 45);
+            this.cmbSelectFolder.Name = "cmbSelectFolder";
+            this.cmbSelectFolder.Size = new System.Drawing.Size(25, 23);
+            this.cmbSelectFolder.TabIndex = 5;
+            this.cmbSelectFolder.Text = "...";
+            this.cmbSelectFolder.UseVisualStyleBackColor = true;
+            this.cmbSelectFolder.Click += new System.EventHandler(this.cmbSelectFolder_Click);
+            // 
+            // btnRunPoE
+            // 
+            this.btnRunPoE.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRunPoE.Location = new System.Drawing.Point(217, 74);
+            this.btnRunPoE.Name = "btnRunPoE";
+            this.btnRunPoE.Size = new System.Drawing.Size(75, 23);
+            this.btnRunPoE.TabIndex = 6;
+            this.btnRunPoE.Text = "Run PoE";
+            this.btnRunPoE.UseVisualStyleBackColor = true;
+            this.btnRunPoE.Click += new System.EventHandler(this.btnRunPoE_Click);
+            // 
+            // btnDelProfile
+            // 
+            this.btnDelProfile.Enabled = false;
+            this.btnDelProfile.Location = new System.Drawing.Point(405, 2);
+            this.btnDelProfile.Name = "btnDelProfile";
+            this.btnDelProfile.Size = new System.Drawing.Size(96, 34);
+            this.btnDelProfile.TabIndex = 7;
+            this.btnDelProfile.Text = "Delete selected profile";
+            this.btnDelProfile.UseVisualStyleBackColor = true;
+            this.btnDelProfile.Click += new System.EventHandler(this.btnDelProfile_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(508, 101);
+            this.Controls.Add(this.btnDelProfile);
+            this.Controls.Add(this.btnRunPoE);
+            this.Controls.Add(this.cmbSelectFolder);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPoEpath);
+            this.Controls.Add(this.btnCreateProfile);
+            this.Controls.Add(this.cmbProfiles);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "PoE Profile Selector";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbProfiles;
+        private System.Windows.Forms.Button btnCreateProfile;
+        private System.Windows.Forms.TextBox txtPoEpath;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button cmbSelectFolder;
+        private System.Windows.Forms.Button btnRunPoE;
+        private System.Windows.Forms.Button btnDelProfile;
+    }
+}
+
---
+++ PoE Profile Selector/MainForm.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using PoE_Profile_Selector.Properties;
+using System.Diagnostics;
+
+namespace PoE_Profile_Selector
+{
+    public partial class MainForm : Form
+    {
+        private const string BaseName_config="production_Config.ini";
+        private const string BaseName_user="production_login.credentials";
+        private const string PoE_games_folder=@"My Games\Path of Exile";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                string poe_mygames_folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), PoE_games_folder);
+                bool poe_main_config_exist = false;
+                DirectoryInfo poe_configs = new DirectoryInfo(poe_mygames_folder);
+                foreach (FileInfo curr_file_info in poe_configs.GetFiles("*.ini", SearchOption.TopDirectoryOnly))
+                {
+                    if (curr_file_info.Name == BaseName_config) poe_main_config_exist = true;
+                    if (curr_file_info.Name.Contains("production_Config_") && File.Exists(curr_file_info.FullName.Replace("ini", "credentials").Replace("_Config_","_login_")))
+                    {
+                        string curr_profile_name = curr_file_info.Name.Replace("production_Config_", "").Replace(".ini","");
+                        cmbProfiles.Items.Add(curr_profile_name);
+                    }
+                }
+
+                if (!poe_main_config_exist)
+                {
+                    MessageBox.Show("Path of Exile main config file not found! Save PoE setting in game and check again.", "Main config not found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    Application.Exit();
+                }
+
+                if (Settings.Default.PoEexePath != null || Settings.Default.PoEexePath != "")
+                {
+                    txtPoEpath.Text = Settings.Default.PoEexePath;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                return;
+            }
+        }
+
+        private void cmbSelectFolder_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog select_file = new OpenFileDialog();
+                select_file.CheckFileExists = true;
+                select_file.Multiselect = false;
+                select_file.Title = "Select PoE main exe file...";
+                select_file.ValidateNames = true;
+                select_file.Filter = "Exe files(*.exe)|*.exe";
+                if (select_file.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    txtPoEpath.Text = select_file.FileName;
+                    Settings.Default.PoEexePath = txtPoEpath.Text;
+                    Settings.Default.Save();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while selecting file: " + ex.Message, "Selection error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void cmbProfiles_TextChanged(object sender, EventArgs e)
+        {
+            if (cmbProfiles.Text.Length > 0)
+            {
+                if (cmbProfiles.Items.Contains(cmbProfiles.Text))
+                {
+                    //profile name already exist
+                    btnCreateProfile.Enabled = false;
+                    btnDelProfile.Enabled = true;
+                }
+                else
+                {
+                    //new profile name
+                    btnCreateProfile.Enabled = true;
+                    btnDelProfile.Enabled = false;
+                }
+            }
+            else
+            {
+                //empty profile name
+                btnCreateProfile.Enabled = false;
+                btnDelProfile.Enabled = false;
+            }
+        }
+
+        private void btnCreateProfile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string new_profile_name=cmbProfiles.Text;
+                string poe_mygames_folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), PoE_games_folder) + @"\";
+
+                //copy main ini file as "production_Config_<profilename>.ini"
+                File.Copy(poe_mygames_folder + BaseName_config, poe_mygames_folder + "production_Config_" + cmbProfiles.Text + ".ini", true);
+
+                //copy user data file as "production_Login_<profilename>.credentials"
+                File.Copy(poe_mygames_folder + BaseName_user, poe_mygames_folder + "production_login_" + cmbProfiles.Text + ".credentials", true);
+                
+                cmbProfiles.Items.Add(new_profile_name);
+                cmbProfiles.SelectedItem = new_profile_name;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while creating profile: " + ex.Message, "Profile creation error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void cmbProfiles_SelectedValueChanged(object sender, EventArgs e)
+        {
+            if (cmbProfiles.Items.Contains(cmbProfiles.Text)) btnDelProfile.Enabled = true; else btnDelProfile.Enabled = false;
+        }
+
+        private void btnDelProfile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string poe_mygames_folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), PoE_games_folder) + @"\";
+                string curr_profile_name = cmbProfiles.Text;
+                if (cmbProfiles.Items.Contains(cmbProfiles.Text))
+                {
+                    File.Delete(poe_mygames_folder + "production_Config_" + curr_profile_name + ".ini");
+                    File.Delete(poe_mygames_folder + "production_login_" + curr_profile_name + ".credentials");
+                    cmbProfiles.Items.Remove(curr_profile_name);
+                    if (cmbProfiles.Items.Count > 0)
+                    {
+                        cmbProfiles.SelectedIndex = 0;
+                    }
+                    else
+                    {
+                        cmbProfiles.Text = "";
+                        btnDelProfile.Enabled = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while deleting profile: " + ex.Message, "Profile deletion error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+
+        private void btnRunPoE_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (File.Exists(txtPoEpath.Text))
+                {
+                    //check if any profile selected
+                    if (cmbProfiles.SelectedIndex >= 0)
+                    {
+                        string poe_mygames_folder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), PoE_games_folder) + @"\";
+
+                        //some exist profile selected
+                        //copy "production_Config_<profilename>.ini" to main ini file
+                        File.Copy(poe_mygames_folder + "production_Config_" + cmbProfiles.Text + ".ini", poe_mygames_folder + BaseName_config, true);
+
+                        //copy "production_Login_<profilename>.credentials"to user data file
+                        File.Copy(poe_mygames_folder + "production_login_" + cmbProfiles.Text + ".credentials", poe_mygames_folder + BaseName_user, true);
+                    }
+                    
+                    //run PoE exe file
+                    Process PoE_exe = new Process();
+                    PoE_exe.StartInfo.UseShellExecute = true;
+                    PoE_exe.StartInfo.WorkingDirectory = (new FileInfo(txtPoEpath.Text)).DirectoryName;
+                    PoE_exe.StartInfo.FileName = txtPoEpath.Text;
+
+                    PoE_exe.Start();
+                }
+                else
+                {
+                    MessageBox.Show("PoE exe file not exist!", "File not found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while running PoE" + ex.Message, "Run error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+        }
+    }
+}
